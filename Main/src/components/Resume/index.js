b0c7a9460a005c8831b68792c218797583c10840
Randy DeCent <109132522+RandyDeCent@users.noreply.github.com>
2023-01-30 12:49:27 -0800

    Update index.js

--- Main/src/components/Resume/index.js
+++ Main/src/components/Resume/index.js
@@ -5,27 +5,42 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://github.com/RandyDeCent">resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
           <li>HTML</li>
+
           <li>CSS</li>
+
           <li>JavaScript</li>
+
           <li>jQuery</li>
+
           <li>responsive design</li>
+
           <li>React</li>
+
           <li>Bootstrap</li>
+
         </ul>
+
         <h3>Back-end Proficiencies</h3>
         <ul className="skills">
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
+          <li>APIs calls</li>
+
+          <li>Node.js</li>
+
+          <li>Express.js</li>
+
+          <li>MySQL noSQL</li>
+
+          <li>Mongoose</li>
+
           <li>REST</li>
+          
           <li>GraphQL</li>
+
         </ul>
       </div>
     </section>
@@ -33,3 +48,4 @@ function Resume() {
 }
 
 export default Resume;
+
